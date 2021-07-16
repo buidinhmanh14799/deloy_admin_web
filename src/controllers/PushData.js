84e41bbb2c35bc41acd6464ae89cae1f88a3e38f
bui dinh manh <buidinhmanh14799@gmail.com>
2021-07-16 23:00:26 +0700

    manh

--- src/controllers/PushData.js
+++ src/controllers/PushData.js
@@ -31,7 +31,7 @@ export const pushOnlinePractice = (data) => {
     });
 }
 export const pushOnlinePracticejson = (data) => {
-    // console.log("======>" + JSON.stringify(data));
+    console.log(HEADER);
     return new Promise((resolve, reject) => {
         fetch(`${process.env.REACT_APP_API_URL}/practiceonline/data_json`, {
             method: 'POST',
